c8b79bda24a4852f65a5a4775fa110d1d5b348ec
parkyongsang <dooly5879@naver.com>
2022-10-25 21:11:06 +0900

    chapter1-출력

---
+++ src/main/java/J01_출력/PrintTest1.java
@@ -0,0 +1,27 @@
+package J01_출력;
+
+/**
+ *
+ */
+public class PrintTest1 {
+    public static void main(String[] args) {
+        // 한 줄 주석
+        /*
+        여러줄 주석
+         */
+        //pvsm, pvsm ctrl+sapcebar
+        /**
+         *
+         * 클래스 또는 메소드의 역활을 명시 설명하기 위한 주석
+         */
+
+        //문자열 출력
+        System.out.print("이름: ");
+        System.out.print("변상원 ");
+        System.out.print("주소: ");
+        System.out.println("부산 해운대구 우동");
+
+        boolean isUsernameCheckFlag = true;
+
+    }
+}
